9390a8c55d2f42732e18576bb9e94162d328cc88
Fitria Novyanti <114640363+fitrinyc@users.noreply.github.com>
2022-10-07 17:10:13 +0700

    Add files via upload
    Saya sudah mengumpulkan tugas praktikum 1

---
+++ Tugas_Praktikum_Pertemuan1_Tugas 1.cpp
@@ -0,0 +1,28 @@
+ #include <iostream>
+using namespace std;
+
+int main(){
+
+//TUGAS 1
+    string Nama = "Fitria Novyanti Taufik",
+            NPM = "2210631170021",
+          Kelas = "1C Informatika",
+           Hobi = "Mendengarkan Musik dan Nonton drakor",
+       CitaCita = "Mau menjadi seperti Nam Do-san";
+    int    Umur = 17;
+
+
+    cout << "---------------------------------------------" << endl << endl;
+    cout << "               IDENTITAS DIRI                " << endl;
+    cout << "---------------------------------------------" << endl << endl;
+    cout << "  Nama       : " << Nama                       << endl;
+    cout << "  NPM        : " << NPM                        << endl;
+    cout << "  Umur       : " << Umur                       << endl;
+    cout << "  Kelas      : " << Kelas                      << endl;
+    cout << "  Hobi       : " << Hobi                       << endl;
+    cout << "  Cita Cita  : " << CitaCita                   << endl;
+    cout << "---------------------------------------------" << endl << endl;
+
+
+return 0;
+}
